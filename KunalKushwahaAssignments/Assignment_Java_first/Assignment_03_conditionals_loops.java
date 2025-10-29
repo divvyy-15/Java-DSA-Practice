823cd7d71a00c118663f7627e27a346f3f1095c7
Divyanshi Sharma <divshrma15@gmail.com>
2025-10-29 14:55:55 +0530

    Next 3 ques in 3rd Assignment

--- KunalKushwahaAssignments/Assignment_Java_first/Assignment_03_conditionals_loops.java
+++ KunalKushwahaAssignments/Assignment_Java_first/Assignment_03_conditionals_loops.java
@@ -1,17 +1,19 @@
 package com.practice;
 
+import java.util.Scanner;
+
 public class Assignment_03_conditionals_loops {
 
     public static void main(String[] args) {
 
 //1. Print all 4 digit armstrong number
-        /*for(int i=1000;i<10000;i++)
+        for(int i=1000;i<10000;i++)
         {
             if(isArmstrongNum(i))
             {
                 System.out.print(i + " ");
             }
-        }*/
+        }
 
 //2. Given an integer number n, return the difference between the product of its digits and the sum of its
 // digits
@@ -21,10 +23,14 @@ public class Assignment_03_conditionals_loops {
         factorsOfANumber(48);
 
 //4. Take integer inputs till the user enters 0 and print the sum of all numbers (HINT: while loop)
+        System.out.println("The total sum of all entered numbers is: "+keepPrintingSumOfEnteredNumbers());
 
 //5. Take integer inputs till the user enters 0 and print the largest number from all.
+        System.out.println("The largest number from all entered numbers is: "+largestOfAllEnteredNumbers());
 
 //6. Factorial Program In Java
+        int n = 10;
+        System.out.println("Factorial of "+ n + " is: "+factorialOfANumber(n));
 
 //7. Calculate Average Of N Numbers
 
@@ -91,5 +97,56 @@ public class Assignment_03_conditionals_loops {
         {
             System.out.print(", "+ n);
         }
+    }
+
+    public static int keepPrintingSumOfEnteredNumbers()
+    {
+        int sum = 0;
+        Scanner inp = new Scanner(System.in);
+        while(true)
+        {
+            System.out.println("Enter any number(when 0 is entered program stops): ");
+            int n = inp.nextInt();
+            if(n==0)
+            {
+                break;
+            }
+            else
+            {
+                sum = sum + n;
+            }
+        }
+        return sum;
+    }
+
+    public static int largestOfAllEnteredNumbers()
+    {
+        int largest = Integer.MIN_VALUE;
+        Scanner inp = new Scanner(System.in);
+        while(true){
+            System.out.println("Enter any number(when 0 is entered program stops): ");
+            int n = inp.nextInt();
+            if(n==0)
+            {
+                break;
+            }
+            else
+            {
+                if(n>largest)
+                {
+                    largest = n;
+                }
+            }
+        }
+        return largest;
+    }
+
+    public static int factorialOfANumber(int num)
+    {
+        int fact = 1;
+        for(int i=1;i<=num;i++) {
+            fact = fact * i;
+        }
+        return fact;
     }
 }
