198169481e12ec4d1643ef1a08c704242f9d6c0b
Divyanshi Sharma <divshrma15@gmail.com>
2025-12-15 21:48:50 +0530

    Easy level questions from LeetCode(Q5-12):Arrays

--- KunalKushwahaAssignments/Assignment_Java_first/leetCodeArrays.java
+++ KunalKushwahaAssignments/Assignment_Java_first/leetCodeArrays.java
@@ -25,6 +25,65 @@ public class leetCodeArrays {
 // ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
 //Specifically, ans is the concatenation of two nums arrays.
 //Return the array ans.
+        int[] nums3 = {1,2,1}; //[1,2,1,1,2,1]
+        System.out.println("Resultant array: "+Arrays.toString(getConcatenation(nums3)));
+
+//5.You are given an m x n integer grid accounts where accounts[i][j] is the amount of money the ith customer
+// has in the jth bank. Return the wealth that the richest customer has.
+//A customer's wealth is the amount of money they have in all their bank accounts.
+// The richest customer is the customer that has the maximum wealth.
+        int[][] accounts = {{1,2,3},{3,2,1}};
+        System.out.println("Maximum wealth is: "+maximumWealth(accounts));
+
+//6.There are n kids with candies. You are given an integer array candies, where each candies[i] represents
+// the number of candies the ith kid has, and an integer extraCandies, denoting the number of extra candies
+// that you have.
+//Return a boolean array result of length n, where result[i] is true if, after giving the ith kid all the
+// extraCandies, they will have the greatest number of candies among all the kids, or false otherwise.
+//Note that multiple kids can have the greatest number of candies.
+        int[] candies = {2,3,5,1,3};
+        int extraCandies  = 3;
+        System.out.println("Result: "+Arrays.toString(kidsWithCandies(candies,extraCandies)));
+
+//7.Given an array of integers nums, return the number of good pairs.
+//A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+        int[] nums4 = {1,2,3,1,1,3};
+        System.out.println("Number of good pairs: "+goodPairsCount(nums4));
+
+//8.Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it.
+// That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
+//Return the answer in an array.
+        int[] nums5 = {8,1,2,2,3};
+        System.out.println("Result: "+Arrays.toString(smallerNumbersThanCurrent(nums5)));
+
+//9.Given an array nums of integers, return how many of them contain an even number of digits.
+        int[] nums6 = {12,345,2,6,7896};
+        System.out.println("Total numbers with even no. of digits in the array: "+findNumbers(nums6));
+
+//10.Given an array arr of integers, check if there exist two indices i and j such that:
+//i != j
+//0 <= i, j < arr.length
+//arr[i] == 2 * arr[j]
+        int[] nums7 = {3,1,7,11};
+        System.out.println("There exists a number whose double is also present in the array: "+checkIfExist(nums7));
+
+//11.Given an array of integers nums and an integer target, return indices of the two numbers such that they
+// add up to target.You may assume that each input would have exactly one solution, and you may not use the
+// same element twice. You can return the answer in any order.
+        int[] nums8 = {2,7,11,15};
+        System.out.println("Indices are: "+Arrays.toString(twoSum(nums8,9)));
+
+//12.Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that
+// each unique element appears only once. The relative order of the elements should be kept the same.
+//Consider the number of unique elements in nums to be k. After removing duplicates, return the number of
+// unique elements k.The first k elements of nums should contain the unique numbers in sorted order.
+// The remaining elements beyond index k - 1 can be ignored.
+        int[] nums9 = {0,0,1,1,1,2,2,3,3,4};
+        System.out.println("Number uf unique elements are: "+removeDuplicates(nums9));
+
+//13.
+
+
     }
 
     public static int[] runningSum(int[] a)
@@ -64,5 +123,173 @@ public class leetCodeArrays {
             result[i] = a[a[i]];
         }
         return result;
+    }
+
+    public static int[] getConcatenation(int[] a)
+    {
+        int n = a.length;
+        int[] result = new int[2*n];
+        for(int i=0;i<n;i++)
+        {
+            result[i] = a[i];
+            result[i+n] = a[i];
+        }
+        return result;
+    }
+
+    public static int maximumWealth(int[][] a)
+    {
+        int m = a.length;
+        //int n = a[0].length;
+        int[] wealth = new int[m];
+        int sum = 0;
+        int index = 0;
+        for(int i=0;i<a.length;i++)//0,1
+        {
+            sum=0;
+            for(int j=0;j<a[i].length;j++)//0,1,2
+            {
+                sum = sum + a[i][j];
+            }
+            wealth[index] = sum;
+            index++;
+        }
+        int max=Integer.MIN_VALUE;
+        for(int k=0;k<wealth.length;k++)
+        {
+            if(wealth[k]>=max)
+            {
+                max = wealth[k];
+            }
+        }
+        return max;
+    }
+
+    public static boolean[] kidsWithCandies(int[] a,int n)
+    {
+        boolean[] result = new boolean[a.length];
+        int max = Integer.MIN_VALUE;
+        int[] extraCandiesGiven = new int[a.length];
+        for(int i=0;i<a.length;i++)
+        {
+            extraCandiesGiven[i] = a[i]+n;
+        }
+        for(int i=0;i<a.length;i++)
+        {
+            if(a[i]>=max)
+            {
+                max = a[i];
+            }
+            if(extraCandiesGiven[i]>=max)
+            {
+                result[i] = true;
+            }
+        }
+        return result;
+    }
+
+    public static int goodPairsCount(int[] a)
+    {
+        int count = 0;
+        for(int i=0;i<a.length;i++)
+        {
+            for(int j=i+1;j<a.length;j++)
+            {
+                if(a[i]==a[j])
+                {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static int[] smallerNumbersThanCurrent(int[] a)
+    {
+        int[] result = new int[a.length];
+        int index = 0;
+        for(int i=0;i<a.length;i++)
+        {
+            int count = 0;
+            for(int j=0;j<a.length;j++)
+            {
+                if(a[i]>a[j] && i!=j)
+                {
+                    count++;
+                }
+            }
+            result[index] = count;
+            index++;
+        }
+        return result;
+    }
+
+    public static int findNumbers(int[] a)
+    {
+        int count = 0;
+        int result = 0;
+        for(int i=0;i<a.length;i++)
+        {
+            count = 0;
+            int num = a[i];
+            while(num>0)
+            {
+                int rem = num%10;
+                num = num/10;
+                count++;
+            }
+            if(count%2==0)
+            {
+                result++;
+            }
+        }
+        return result;
+    }
+
+    public static boolean checkIfExist(int[] a)
+    {
+        for(int i=0;i<a.length;i++)
+        {
+            for(int j=0;j<a.length;j++)
+            {
+                if(a[i]==a[j]*2 && i!=j)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static int[] twoSum(int[] a,int target)
+    {
+        int[] result = new int[2];
+        for(int i=0;i<a.length;i++)
+        {
+            for(int j=0;j<a.length;j++)
+            {
+                if(a[i]+a[j]==target && i!=j)
+                {
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
+                }
+            }
+        }
+        return null;
+    }
+
+    public static int removeDuplicates(int[] a)
+    {
+        int writePos = 1;
+        for(int i=1;i<a.length;i++)
+        {
+            if(a[i]!=a[i-1])
+            {
+                a[writePos]=a[i];
+                writePos++;
+            }
+        }
+        return writePos;
     }
 }
