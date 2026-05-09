d7c10df5d8233bf1b7ec6bb785110d85538d0232
Divyanshi Sharma <divshrma15@gmail.com>
2026-05-09 13:26:54 +0530

    May 8,9 - feat: Implement Check if two given strings are anagram, group anagrams from a list of Strings and Sort a Map by it's values

--- SDET-Interview-Practice/Week3_FrequencyMaps.java
+++ SDET-Interview-Practice/Week3_FrequencyMaps.java
@@ -41,6 +41,22 @@ public class Week3_FrequencyMaps {
         //Que 9: Remove duplicate words from the string
         String s4 = "alpha beta alpha";
         System.out.println("After removing duplicate words: "+removeDuplicateWords(s4));
+
+        //Que 10: Check if the given Strings are valid anagrams
+        String s5 = "Listen";
+        String s6 = "Silent";
+        System.out.println("The given input Strings are Anagrams? "+areAnagrams(s5,s6));
+
+        //Que 11: Group the Anagrams
+        String[] arr7 = {"eat", "tea", "tan"};
+        System.out.println("Anagrams grouped: "+groupAnagrams(arr7));
+
+        //Que 12: Sort Map by value
+        HashMap<Character,Integer> hm = new HashMap<>();
+        hm.put('A',3);
+        hm.put('B',1);
+        hm.put('C',2);
+        System.out.println("After sorting: "+sortMapByValue(hm));
     }
 
     public static HashMap<Character, Integer> charOccCount(String s)
@@ -257,5 +273,93 @@ public class Week3_FrequencyMaps {
             sb.append(word).append(" ");
         }
         return sb.toString().trim();
+    }
+
+    public static boolean areAnagrams(String s1,String s2)
+    {
+        //1.Check if string is null, empty
+        if(s1==null || s2==null || s1.isEmpty() || s2.isEmpty())
+        {
+            throw new IllegalArgumentException("Input Strings are empty or null!");
+        }
+        //2.Check whether the length of both the strings are same
+        if(s1.length()!=s2.length())
+        {
+            return false;
+        }
+        //3.Normalise the case of both the strings
+        s1 = s1.toLowerCase();
+        s2 = s2.toLowerCase();
+        //4.Initialize a map to store characters and their counts
+        HashMap<Character,Integer> hm = new HashMap<>();
+        //5.Loop through first string to store chars and count in the map [Add]
+        for(int i=0;i<s1.length();i++)
+        {
+            char c = s1.charAt(i);
+            hm.put(c,hm.getOrDefault(c,0)+1);
+        }
+        //6.Loop through second string [Subtract]
+        for(int i=0;i<s2.length();i++)
+        {
+            char c = s2.charAt(i);
+            if(!hm.containsKey(c))
+            {
+                return false; //Character doesn't exist in s1 at all
+            }
+            else {
+                hm.put(c,hm.get(c)-1);
+                if(hm.get(c)<0)
+                {
+                    return false;
+                }
+            }
+        }
+        //7.If length was equal and both the strings had exact same characters - it is an anagram!
+        return true;
+    }
+
+    public static ArrayList<List<String>> groupAnagrams(String[] s)
+    {
+        //1.Check if the array is null or empty
+        if(s==null || s.length==0)
+        {
+            throw new IllegalArgumentException("Input array is null or empty!");
+        }
+        //2.Declare a Map of String,List of string
+        HashMap<String, List<String>> hm = new HashMap<>();
+        //3.Loop through word in your array
+        for(String w:s)
+        {
+            //4.Convert the word to character array and sort it
+            char[] alphabets = w.toCharArray();
+            Arrays.sort(alphabets);
+            //5.Then turn it back to string - becomes key
+            String key = new String(alphabets); // This turns [a, e, t] into "aet"
+            //6.Populate the map, if the sorted word isn't there, put empty arraylist there and
+            if(!hm.containsKey(key))
+            {
+                hm.put(key,new ArrayList<>());
+            }
+            hm.get(key).add(w); // Add the ORIGINAL word (w)
+        }
+        //7.Return only the groups (values)
+        return new ArrayList<>(hm.values());
+    }
+
+    public static LinkedHashMap<Character, Integer> sortMapByValue(HashMap<Character,Integer> hm)
+    {
+        //1.Convert the entrySet to a list
+        List<Map.Entry<Character,Integer>> entrySetList = new ArrayList<>(hm.entrySet());
+        //2.Sort this list based on values using comparator
+        Collections.sort(entrySetList,(e1,e2) ->e1.getValue().compareTo(e2.getValue()));
+        //3.Initialize a linkedHashMap to remember the order
+        LinkedHashMap<Character,Integer> result = new LinkedHashMap<>();
+        //4.Loop through the list and put each pair in the linkedHashMap
+        for(Map.Entry<Character,Integer> e:entrySetList)
+        {
+            result.put(e.getKey(),e.getValue());
+        }
+        //5.Return it
+        return result;
     }
 }
